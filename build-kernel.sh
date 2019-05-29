ad708a8f0f93de9fe4eb6431cd7ac1b9f33344af
Christian Poessinger <christian@poessinger.com>
2019-05-29 19:37:18 +0200

    build-kernel: use Kernel build command for Debian packages
    The Debian make-kpkg system is deprecated. The Kernel itself ships a proper
    maintained Debian buildsystem which is used for Debian releases after Jessie.

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-RELEASE=`echo $GIT_BRANCH | sed 's/origin\//* /g' |sed -n 's/^\* \(.*\)$/\1/p'`
-
-# newer Kernel versions come with a "# SPDX-License-Identifier: GPL-2.0" identifier
-# as the first line in the file - use compatible approach with grep rather then sed
-VERSION=$(grep "^VERSION" Makefile | grep -Eo '[0-9]{1,4}')
-PATCHLEVEL=$(grep "^PATCHLEVEL" Makefile | grep -Eo '[0-9]{1,4}')
-SUBLEVEL=$(grep "^SUBLEVEL" Makefile | grep -Eo '[0-9]{1,4}')
 ARCH=$(dpkg --print-architecture)
 
 case "$ARCH" in
     amd64)
         make x86_64_vyos_defconfig
-        TARGETS="kernel_source kernel_debug kernel_headers kernel_image"
-        # the following targets are not supported for Linux Kernels > 4.14 as
-        # they have been removed from the Makefile (commits 18afab8c1d3c2 &
-        # 22cba31bae9dc).
-        if [ ${PATCHLEVEL} -lt 14 ]; then
-            TARGETS+=" kernel_manual kernel_doc"
-        fi
-        echo "$VERSION.$PATCHLEVEL.$SUBLEVEL-amd64-vyos" > ../kernel-version
-        LOCALVERSION="" make-kpkg --rootcmd fakeroot --initrd --append_to_version -amd64-vyos --revision=$VERSION.$PATCHLEVEL.$SUBLEVEL-0+vyos+current0 ${TARGETS} -j$(cat /proc/cpuinfo | grep processor | wc -l)
+        echo $(make kernelversion)-amd64-vyos > ../kernel-version
+        make bindeb-pkg LOCALVERSION='-amd64-vyos' KDEB_PKGVERSION=$(make kernelversion)-1 -j $(getconf _NPROCESSORS_ONLN)
     ;;
 
     armhf)
