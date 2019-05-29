9fcfef5ce19fb25c50a0cffdd5203795b8b72274
Christian Poessinger <christian@poessinger.com>
2019-05-29 17:23:47 +0200

    build-kernel: remove commented curl command

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 RELEASE=`echo $GIT_BRANCH | sed 's/origin\//* /g' |sed -n 's/^\* \(.*\)$/\1/p'`
 
-#TOKEN=`cat ~/tokennr`
-#REPO="vyos-kernel"
-
-#curl -XPOST -H "Authorization: token $TOKEN" https://api.github.com/repos/vyos/$REPO/statuses/$(git rev-parse HEAD) -d "{
-#  \"state\": \"pending\",
-#  \"target_url\": \"${BUILD_URL}\",
-#  \"description\": \"The build is pending!\",
-#  \"context\": \"continuous-integration/jenkins\"
-#}"
-
 # newer Kernel versions come with a "# SPDX-License-Identifier: GPL-2.0" identifier
 # as the first line in the file - use compatible approach with grep rather then sed
 VERSION=$(grep "^VERSION" Makefile | grep -Eo '[0-9]{1,4}')
